0699feb776255f96cfa0482393c477306f2ef8be
ligior <ligior74@gmail.com>
2024-02-10 01:43:46 +0500

    first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://b1e2f1d6-4a5f-4d39-805f-22c98963282f.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
